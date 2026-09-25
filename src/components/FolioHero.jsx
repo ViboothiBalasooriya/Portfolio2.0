88cce837ec4d98df12c0232915f918594f2a332c
ViboothiBalasooriya <viboothibalasuriya@gmail.com>
2026-09-25 23:10:41 +0530

    Move the entire right text block slightly up in Hero section

--- src/components/FolioHero.jsx
+++ src/components/FolioHero.jsx
@@ -333,7 +333,7 @@ const FolioHero = () => {
             alignItems: 'flex-end', // Aligned content right
             textAlign: 'right', // Aligned text right
             maxWidth: '800px',
-            marginTop: '68vh', // Moved further down again
+            marginTop: '66vh', // Moved slightly up again
             paddingRight: '5%', // Padding on the right instead of left
           }}
         >
